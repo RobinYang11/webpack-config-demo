a85f813b0f2795a340bbe467157e2a7681bbe1c9
robinyang <756517553@qq.com>
2022-03-30 17:36:55 +0800

    Feat

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as  ReactDOM from 'react-dom/client';
-
 import App from './App';
 import './home.less';
 
+// react18 启用 reactDom.render()
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 
